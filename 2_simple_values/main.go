dc328792ec28e3e2e1f161395b74808a481bde5b
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-19 23:30:46 +0530

    Add Go values example

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+	// int
+	fmt.Println(1)
+
+	// string
+	fmt.Println("Hello, World!")
+	
+	// bool
+	fmt.Println(true)
+
+	// float
+	fmt.Println(3.14)
+
+	// Arithmetic operations
+	fmt.Println(7.0 / 3.0) // division
+}
