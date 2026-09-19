043ed4649549d320304759f9bd49a132c69de514
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-19 23:41:55 +0530

    Add variables example

---
+++ 3_variables/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var name string = "golang"
+	// var name = "golang"
+
+	fmt.Println(name)
+
+	var isAdult = true
+	fmt.Println(isAdult)
+
+	var age int = 30
+	fmt.Println(age)
+
+	var height float64 = 1.75
+	fmt.Println(height)
+
+	// short variable declaration
+	country := "USA"
+	fmt.Println(country)
+
+	// shorthand declaration with multiple variables
+	// name1 := "Alice"
+	// fmt.Println(name1)
+
+	// x, y := 10, 20
+	// fmt.Println(x, y)
+
+
+	// var myname string
+
+	// myname = "John Doe"
+
+	// fmt.Println(myname)
+
+
+	// var price float32 = 19.99
+	// var price = 19.99
+	price := 19.99
+	fmt.Println(price)
+}
