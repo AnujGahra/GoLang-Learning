4f2c37feb41a9169703f75337a7fe13088267183
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 00:49:41 +0530

    Add if-else examples

---
+++ 6_if_else/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 12
+
+	if age >= 18 {
+		fmt.Println("You are an adult.")
+	} else if age >= 13 && age < 18 {
+		fmt.Println("You are a teenager.")
+	} else {
+		fmt.Println("You are a child.")
+	}
+
+
+
+	var role = "admin"
+	var hashPermission = true
+
+	if role == " admin" && hashPermission {
+		fmt.Println("You have access to the admin panel.")
+	} else {
+		fmt.Println("You do not have access to the admin panel.")
+	}
+}
