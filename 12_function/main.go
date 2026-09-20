fd6d252dfaae98448f71ff3e4b7ee0a6b6409793
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:20:03 +0530

    Add basic function examples in Go

--- 12_function/main.go
+++ 12_function/main.go
@@ -12,11 +12,19 @@ func add2(a, b int) int {
 	return a + b
 }
 
+func getLanguages() (string, string, string) {
+	return "golang", "python", "java"
+}
+
 func main() {
 	result := add(5, 3)
 	fmt.Println(result)
 
 	result2 := add2(10, 20)
 	fmt.Println(result2)
+
+	language1, language2, language3 := getLanguages()
+	fmt.Println(language1, language2, language3)
+	fmt.Println(getLanguages())
 }
 
