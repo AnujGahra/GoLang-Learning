8e7aec827c8a7abcad54b28d10c5aa7da877ed1f
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 12:23:56 +0530

    Practice map declaration and initialization in Go

--- 10_maps/main.go
+++ 10_maps/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	// creating a map
+
+	m := make(map[string]string)
+
+	// setting values in a map
+	m["name"] = "John"
+	m["age"] = "30"
+	m["city"] = "New York"
+
+	fmt.Println(m)
+	fmt.Println(m["name"])
 	
+
+	// getting values from a map
+	name := m["name"]
+	age := m["age"]
+	city := m["city"]
+
+	println(name)
+	println(age)
+	println(city)
 }
