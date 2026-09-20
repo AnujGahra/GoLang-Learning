b3ef0f08a3493c5dc49bdbeae158cf126b1a0bf7
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:39:06 +0530

    Explore closure behavior in Go

--- 14_closures/main.go
+++ 14_closures/main.go
@@ -1,10 +1,24 @@
 package main
 
+import "fmt"
+
+
+func counter() func() int {
+	var count int = 0
+	return func() int {
+		count++
+		return count
+	}
+}
+
 
 func main() {
 	// Closure is a function value that references variables from outside its body.
 	// The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
 
-
+	c := counter()
+	fmt.Println(c()) // 1
+	fmt.Println(c()) // 2
+	fmt.Println(c()) // 3
 
 }
