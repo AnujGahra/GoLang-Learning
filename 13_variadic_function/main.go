3c7ddb467e30f76b05f676d4b82885881baac3a7
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:35:20 +0530

    Practice passing multiple arguments to variadic functions

--- 13_variadic_function/main.go
+++ 13_variadic_function/main.go
@@ -18,4 +18,8 @@ func main() {
 
 	result := sum(1, 2, 3, 4, 5)
 	fmt.Println("The sum is:", result)
+
+	nums := []int{10, 20, 30, 40, 50}
+	result2 := sum(nums...)
+	fmt.Println("The sum is:", result2)
 }
