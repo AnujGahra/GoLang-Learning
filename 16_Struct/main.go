6a5e20488b9a46ce1420c73abd2da1d1e5faae87
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:56:08 +0530

    Add basic struct examples in Go

--- 16_Struct/main.go
+++ 16_Struct/main.go
@@ -7,9 +7,16 @@ type order struct {
 	id string
 	amount float64
 	status string
-	createdAt time.Time
+	createdAt time.Time // nanoseconds precision
 }
 
 func main() {
+
+	order1 := order{
+		id: "1",
+		amount: 100.0,
+		status: "pending",
+		createdAt: time.Now(),
+	}
 
 }
