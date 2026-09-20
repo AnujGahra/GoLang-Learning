1072891e4c1d9df9e372eba0ec4e566634a52518
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:34:28 +0530

    Practice variadic function parameters in Go

--- 13_variadic_function/main.go
+++ 13_variadic_function/main.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
+
+
+func sum(nums ...int) int {
+	total := 0
+
+	for _, num := range nums {
+		total += num
+	}
+
+	return total
+}
 
 func main() {
 
-	
+	result := sum(1, 2, 3, 4, 5)
+	fmt.Println("The sum is:", result)
 }
