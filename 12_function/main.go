8cd07da0b3fb4833939fcbd9907a07ac93201fe4
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:18:33 +0530

    Practice function declaration and calling in Go

--- 12_function/main.go
+++ 12_function/main.go
@@ -7,8 +7,16 @@ func add(a int, b int) int {
 	return a + b
 }
 
+// 2nd method 
+func add2(a, b int) int {
+	return a + b
+}
+
 func main() {
 	result := add(5, 3)
 	fmt.Println(result)
+
+	result2 := add2(10, 20)
+	fmt.Println(result2)
 }
 
