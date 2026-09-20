5a6778c2e0a25d69b03e5b295524c237fef64729
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-20 22:37:26 +0530

    Learn closures in Go

---
+++ 14_closures/main.go
@@ -0,0 +1,10 @@
+package main
+
+
+func main() {
+	// Closure is a function value that references variables from outside its body.
+	// The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
+
+
+
+}
