d64c113acc8e7be8b11812d7b01e5767d8fb8034
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-21 23:09:42 +0530

    test(interface): add unit tests for learner mock implementation

--- 17_Interface/main.go
+++ 17_Interface/main.go
@@ -7,7 +7,8 @@ type payment struct{}
 
 func (p payment) makePayment(amount float64) {
 	// Implementation for making payment
-	
+	razorpayPaymentGw := razorpay{}
+	razorpayPaymentGw.pay(amount)
 }
 
 type razorpay struct {}
