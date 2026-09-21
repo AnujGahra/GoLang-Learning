dd3a133ddd5d798ce271e37607817790fcbd5e7b
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-21 23:08:54 +0530

    refactor(interface): simplify method signatures in learner interface

--- 17_Interface/main.go
+++ 17_Interface/main.go
@@ -2,6 +2,19 @@ package main
 
 
 
-func main() {
+type payment struct{}
+
+
+func (p payment) makePayment(amount float64) {
+	// Implementation for making payment
 	
+}
+
+type razorpay struct {}
+
+
+func (r razorpay) pay(amount float64) {}
+
+func main() {
+
 }
